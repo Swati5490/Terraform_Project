dae4c71c554664a9f24fe23c2cdc74e6b419446e
Swati5490 <101510518+Swati5490@users.noreply.github.com>
2024-01-08 14:09:51 +0530

    Update main.tf

--- Day1/Project1/main.tf
+++ Day1/Project1/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
         region = "us-east-2"
 }
 
-resource "aws-instance" "aws_example" {
+resource "aws_instance" "aws_example" {
         ami = "ami-05fb0b8c1424f266b"
         instance_type = "t2.micro"
 }
